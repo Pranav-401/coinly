35bb81ecf2b6f560c26f85732af741daaf219991
Utkarsh Kadam <utkarshkadam17@gmail.com>
2025-03-27 16:41:13 +0530

    bug fix

--- components/hero.jsx
+++ components/hero.jsx
@@ -31,9 +31,11 @@ const HeroSection = () => {
   return (
     <div className="pb-20 px-4 bg-black overflow-x-hidden h-[578px]" >
       <div className="container mx-auto text-center ">
-        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title mt-20">
-        <AuroraText>Master Your Money </AuroraText> <br /> <AuroraText>with Smart Insights{" "}</AuroraText>
-        </h1>
+      <h1 className="text-4xl sm:text-5xl md:text-7xl lg:text-[105px] pb-6 gradient-title mt-20 text-center leading-tight">
+  <AuroraText>Master Your Money</AuroraText> <br />
+  <AuroraText>with Smart Insights</AuroraText>
+</h1>
+
         <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
         A smart AI-powered financial platform that helps you track, analyze, and optimize your spending with real-time insights.
         </p>
